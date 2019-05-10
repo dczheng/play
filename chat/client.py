07a708ed8caab6ae50f95d8c497bae0bc5b5e1be
dczheng <dczheng21@outlook.com>
2019-05-10 12:52:00 +0800

    update

--- chat/client.py
+++ chat/client.py
@@ -5,38 +5,6 @@ import sys
 import os
 
 max_len_data = 1024
-address = input( 'server: ' )
-host,port = address.split( ':' )
-port = int(port)
-client = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-try:
-    client.connect( (host,port) )
-except ConnectionRefusedError:
-    print( 'server is closed!' )
-    client.close()
-    exit()
-
-print( "connect to %s"%address )
-
-#if input( 'Creat Account? (N/y)' ) == 'y':
-#    pass
-#else:
-#    #username = input( "username: " )
-#    #password = input( "password: " )
-#    send = "%s+%s"%(username,password)
-#    client.send( send.encode( 'utf-8' ) )
-
-while True:
-    username = input( "username: " )
-    if username == 'q':
-        client.send( 'session_close'.encode( 'utf-8' ) )
-        exit()
-    client.send( username.encode( 'utf-8' ) )
-    r = client.recv( max_len_data ).decode( 'utf-8' )
-    if r == 'Ok':
-        break
-    print( '%s exist!'%username )
-
 def my_recv( client ):
     while True:
         try:
@@ -45,16 +13,55 @@ def my_recv( client ):
             return
         print( '\r' + recv + '\nme: ', end='' )
 
+def main():
 
-t = threading.Thread( target=my_recv, args=(client,) )
-t.start()
+    address = input( 'server: ' )
+    host,port = address.split( ':' )
+    port = int(port)
+    client = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+    try:
+        client.connect( (host,port) )
+    except ConnectionRefusedError:
+        print( 'server is closed!' )
+        client.close()
+        exit()
 
-while True:
+    print( "connect to %s"%address )
 
-    send = input( 'me: ' )
-    if send == 'q':
-        client.send( 'session_close'.encode( 'utf-8' ) )
-        break
-    client.send( send.encode( 'utf-8' ) )
+    #if input( 'Creat Account? (N/y)' ) == 'y':
+    #    pass
+    #else:
+    #    #username = input( "username: " )
+    #    #password = input( "password: " )
+    #    send = "%s+%s"%(username,password)
+    #    client.send( send.encode( 'utf-8' ) )
 
-client.close()
+    while True:
+        username = input( "username: " )
+        if username == 'q':
+            client.send( 'session_close'.encode( 'utf-8' ) )
+            exit()
+        client.send( username.encode( 'utf-8' ) )
+        r = client.recv( max_len_data ).decode( 'utf-8' )
+        if r == 'Ok':
+            break
+        print( '%s exist!'%username )
+
+
+    t = threading.Thread( target=my_recv, args=(client,) )
+    t.start()
+
+    while True:
+
+        send = input( 'me: ' )
+        if send == 'q':
+            client.send( 'session_close'.encode( 'utf-8' ) )
+            break
+        client.send( send.encode( 'utf-8' ) )
+
+    client.close()
+
+
+
+if __name__ == '__main__':
+    main()
